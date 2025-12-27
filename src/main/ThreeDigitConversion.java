3ec958506cf0f40bf91a8e343e1126a1ac27749c
izzyvissy <amirulmahidzar@gmail.com>
2025-12-27 23:15:14 +0800

    Directory Structure

--- src/main/ThreeDigitConversion.java
+++ src/main/ThreeDigitConversion.java
@@ -1,18 +1,19 @@
+// Objective: convert a number from 0 to 999 into words
 public class ThreeDigitConversion {
   public static String convertThreeDigits(int number) {
-
+    
     // base case
     if (number == 0) {
         return "";
     }
     
     // case 1: numbers < 20
-
+    
     // case 2: number < 100
 
     // case 3: number >= 100
 
-    
+
     return "";
   } 
 }
