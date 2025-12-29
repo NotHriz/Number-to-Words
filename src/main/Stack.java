aff850bc78a1ea728d090966880b1cf02575aa12
abas0255 <abas.riz2005@gmail.com>
2025-12-29 21:49:19 +0800

    Add stack implementation with push and pop methods
    Implemented basic stack functionality including push, pop, and isEmpty methods.

--- src/main/Stack.java
+++ src/main/Stack.java
@@ -1,3 +1,22 @@
 public class Stack {
-    
+
+    private int[] data;
+    private int top;
+
+    public Stack(int size) {
+        data = new int[size];
+        top = -1;
+    }
+
+    public void push(int value) {
+        data[++top] = value;
+    }
+
+    public int pop() {
+        return data[top--];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
 }
