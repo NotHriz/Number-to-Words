2c1a9b405695ff5ecee169732c25f148022ec55d
izzyvissy <amirulmahidzar@gmail.com>
2025-12-29 13:27:47 +0800

    branch 1

--- src/main/Stack.java
+++ src/main/Stack.java
@@ -1,3 +1,32 @@
 public class Stack {
-    
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+    }
+
+    public void push(int value){
+        stackArray[++top] = value;
+    }
+
+    public int pop(){
+        return stackArray[top--];
+    }
+
+    public int peek(){
+        return stackArray[top];
+    }
+
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+
+    public boolean isFull(){
+        return (top == maxSize - 1);
+    }
+
 }
--- src/main/ThreeDigitConversion.java
+++ src/main/ThreeDigitConversion.java
@@ -8,11 +8,13 @@ public class ThreeDigitConversion {
     }
     
     // case 1: numbers < 20
-    
+    //if (number < 20) 
+    //  return LanguageData.ONES.get(number);
     // case 2: number < 100
-
+    //if (number < 100)
+    //  return LanguageData.TENS.get(number / 10) + (number % 10 != 0 ? " " + convertThreeDigits(number % 10) : "")
     // case 3: number >= 100
-
+    //return LanguageData.ONES.get(number / 100) + " Hundred" + (number % 100 != 0 ? " " + convertThreeDigits(number % 100) : "");
 
     return "";
   } 
