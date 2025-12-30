2bb29adb57f90d3c6263d366f85087097ed90568
izzyvissy <amirulmahidzar@gmail.com>
2025-12-30 21:45:08 +0800

    Filling LanguageData

--- src/main/LanguageData.java
+++ src/main/LanguageData.java
@@ -1,3 +1,38 @@
+import java.util.HashMap;
 public class LanguageData {
+    public static final HashMap<Integer, String> ONES = new HashMap<>();
+    public static final HashMap<Integer, String> TENS = new HashMap<>();
+
+    static {
+        ONES.put(0, "Zero");
+        ONES.put(1, "One");
+        ONES.put(2, "Two");
+        ONES.put(3, "Three");
+        ONES.put(4, "Four");
+        ONES.put(5, "Five");
+        ONES.put(6, "Six");
+        ONES.put(7, "Seven");
+        ONES.put(8, "Eight");
+        ONES.put(9, "Nine");
+        ONES.put(10, "Ten");
+        ONES.put(11, "Eleven");
+        ONES.put(12, "Twelve");
+        ONES.put(13, "Thirteen");
+        ONES.put(14, "Fourteen");
+        ONES.put(15, "Fifteen");
+        ONES.put(16, "Sixteen");
+        ONES.put(17, "Seventeen");
+        ONES.put(18, "Eighteen");
+        ONES.put(19, "Nineteen");
+
+        TENS.put(2, "Twenty");
+        TENS.put(3, "Thirty");
+        TENS.put(4, "Forty");
+        TENS.put(5, "Fifty");
+        TENS.put(6, "Sixty");
+        TENS.put(7, "Seventy");
+        TENS.put(8, "Eighty");
+        TENS.put(9, "Ninety");
+    }
     
 }
--- src/main/ThreeDigitConversion.java
+++ src/main/ThreeDigitConversion.java
@@ -1,19 +1,20 @@
-// Objective: convert a number from 0 to 999 into words
 public class ThreeDigitConversion {
   public static String convertThreeDigits(int number) {
-    
+    // Objective: convert a number to word from 0 to 999 into words
     // base case
     if (number == 0) {
         return "";
     }
-    
     // case 1: numbers < 20
-    
+    if (number < 20) {
+      return LanguageData.ONES.get(number);
+    }
     // case 2: number < 100
-
+    if (number < 100) {
+      return LanguageData.TENS.get(number / 10) + (number % 10 != 0 ? " " + convertThreeDigits(number % 10) : "");
+    }
     // case 3: number >= 100
-
-
-    return "";
+    return LanguageData.ONES.get(number / 100) + " Hundred" + (number % 100 != 0 ? " " + convertThreeDigits(number % 100) : "");
+    
   } 
 }
